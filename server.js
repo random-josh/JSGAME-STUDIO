cb06ea716745c8a470672e033e1aa62ca7153840
Joshua <your-email@example.com>
2026-04-10 16:42:52 +0100

    fix duplicate PORT declaration

--- server.js
+++ server.js
@@ -45,10 +45,6 @@ app.post("/api/games", upload.single("image"), (req, res) => {
   res.json({ success: true });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
 
 // homepage
 app.get("/", (req, res) => {
